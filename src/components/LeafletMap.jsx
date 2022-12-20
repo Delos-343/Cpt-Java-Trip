1e0982254f39bea76c9d98a005829dad5b482907
fateh-ark <akbarfatih92@gmail.com>
2022-12-20 18:28:19 +0700

    final

--- src/components/LeafletMap.jsx
+++ src/components/LeafletMap.jsx
@@ -2,24 +2,24 @@ import React from 'react'
 import { MapContainer, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
-const center = ['-7.2798189387667795, 112.79663873648443']
+const center =[-7.2798189387667795, 112.79663873648443];
 
 const LeafletMap = () => {
   return (
-    <MapContainer center={center} zoom={15} className="w-full h-full my-4">
-      <TileLayer
-        url="https://api.maptiler.com/maps/hybrid/256/{z}/{x}/{y}.jpg?key=GgI2ThFcdA3XHtKATVgg"
-        attribution='
-          <a href="https://www.maptiler.com/copyright/" target="_blank">
-            &copy; MapTiler
-          </a>
-          <a href="https://www.openstreetmap.org/copyright" target="_blank">
-            &copy; OpenStreetMap
-          </a>'
-      >
-  
-      </TileLayer>
-  </MapContainer>
+    <MapContainer 
+    center={center}
+    zoom={15}
+    className="w-full h-full"
+    >
+
+    <TileLayer
+    url="https://api.maptiler.com/maps/hybrid/256/{z}/{x}/{y}.jpg?key=GgI2ThFcdA3XHtKATVgg"
+    attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+    >
+
+    </TileLayer>
+
+    </MapContainer>
   )
 }
 
