359654f0fb82dac6d9768313f87b17dbd0397385
Delos-343 <mfachry.handoko@gmail.com>
2023-01-07 22:44:24 +0700

    attempt at fixing hosting bug' page not rendering; error code 404 NOT FOUND

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,16 @@ function App() {
   return (
     <>
       <Routes>
-        <Route exact path='/' element={<LoginPage />} />
-        <Route path='/home' element={<HomePage />} />
-        <Route path='/our-routes' element={<OurRoute />} />
-        <Route path='/our-routes/1' element={<Route1 />} />
-        <Route path='/our-routes/2' element={<Route2 />} />
-        <Route path='/our-routes/3' element={<Route3 />} />
-        <Route path='/our-routes/4' element={<Route4 />} />
-        <Route path='/our-routes/5' element={<Route5 />} />
-        <Route path='/our-routes/6' element={<Route6 />} />
-        <Route path='/map' element={<RouteMap />} />
+        <Route exact exact path='/' element={<LoginPage />} />
+        <Route exact path='/home' element={<HomePage />} />
+        <Route exact path='/our-routes' element={<OurRoute />} />
+        <Route exact path='/our-routes/1' element={<Route1 />} />
+        <Route exact path='/our-routes/2' element={<Route2 />} />
+        <Route exact path='/our-routes/3' element={<Route3 />} />
+        <Route exact path='/our-routes/4' element={<Route4 />} />
+        <Route exact path='/our-routes/5' element={<Route5 />} />
+        <Route exact path='/our-routes/6' element={<Route6 />} />
+        <Route exact path='/map' element={<RouteMap />} />
       </Routes>
     </>
   )
